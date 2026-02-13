ed63265f7520e3cc4b38be8626e67727bf6686f4
Andy268 <AndrewMao268@gmail.com>
2026-02-13 17:25:25 +0000

    Fixed image on mock interview page

--- pages/2_-_Mock_Interview.py
+++ pages/2_-_Mock_Interview.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Mock Interview", page_icon="📝", layout="wide")
 col1, col2 = st.columns([1, 4])
 
 with col1:
-    st.image("https://via.placeholder.com/150x150.png?text=Logo", width=150)
+    st.image("assets/logo.jpeg", width=150)
 
 with col2:
     st.title("Mock Interview")
